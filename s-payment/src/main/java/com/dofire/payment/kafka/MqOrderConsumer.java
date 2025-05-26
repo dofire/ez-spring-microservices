7cb3338b4b72d9dd06c07e80346452c8f94b0bca
dofire <trind.work@gmail.com>
2025-05-26 12:42:32 +0700

    feat: divide batch list to utilize VTs efficiently

--- s-payment/src/main/java/com/dofire/payment/kafka/MqOrderConsumer.java
+++ s-payment/src/main/java/com/dofire/payment/kafka/MqOrderConsumer.java
@@ -4,29 +4,72 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class MqOrderConsumer {
 
-    private static int counter = 0;
+    private static final AtomicInteger counter = new AtomicInteger(0);
 
     @KafkaListener(
             topics = "order-events",
             groupId = "payment-group",
             batch = "true"
     )
-    public void consume(List<String> messages, Acknowledgment ack){
-        Thread.ofVirtual().start(()->{
+    public void consume(
+            List<String> messages,
+            Acknowledgment ack
+    ) throws InterruptedException {
+
+        List<Thread> threads = new ArrayList<>();
+
+        var subMessages = splitIntoParts(messages, 3);
+        for(var i = 0; i<subMessages.size(); i++){
+            int finalI = i;
+            threads.add(
+                    Thread.ofVirtual().start(()->process(subMessages.get(finalI)))
+            );
+        }
+
+        for(var thread: threads){
+            thread.join();
+        }
+
+        ack.acknowledge();
+
+    }
+
+    private void process(List<String> messages){
+
             try {
                 Thread.sleep(1000);
-                counter += messages.size();
+                counter.addAndGet(messages.size());
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             System.out.println("<> received " + messages.size() + " orders " + " | " + Thread.currentThread() + " | " + counter);
-            ack.acknowledge();
-        });
     }
+
+
+    public static List<List<String>> splitIntoParts(List<String> input, int parts) {
+        int totalSize = input.size();
+        int baseSize = totalSize / parts;
+        int remainder = totalSize % parts;
+
+        List<List<String>> result = new ArrayList<>();
+        int start = 0;
+
+        for (int i = 0; i < parts; i++) {
+            int size = baseSize + (i < remainder ? 1 : 0);
+            int end = Math.min(start + size, totalSize);
+            result.add(input.subList(start, end));
+            start = end;
+        }
+
+        return result;
+    }
+
 
 }
